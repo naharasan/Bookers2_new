5cc41836f2db20b540984d5806b53bb943e70361
EC2 Default User <ec2-user@ip-172-31-0-104.ec2.internal>
2021-11-27 15:23:03 +0000

    [start]4-10

---
+++ bookers2/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '4b6e597c0b449098486aa5298d6deed544383d3fe591f96b2b764ea7de6c56c741e977a22d7381ebe1340d0a2f289ce1530b0b7a21b6cdd6c23930765cb27fb6'
