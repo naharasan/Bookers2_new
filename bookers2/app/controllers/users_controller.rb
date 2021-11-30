82bc01a1016680bf8eb3d71b77ae8d966b054714
EC2 Default User <ec2-user@ip-172-31-0-104.ec2.internal>
2021-11-30 16:47:26 +0000

    [Update]

--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -1,11 +1,15 @@
 class UsersController < ApplicationController
 
   def index
+    @user = current_user
+    @book = Book.new
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
   end
 
   def edit
